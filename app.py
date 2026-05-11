2fc55a132fb43cc940db14465c0edaf3bab6c92d
Gutha Srinidhi <guthasrinidhi990@gmail.com>
2026-05-11 12:08:17 +0530

    Add deployment config

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3, hashlib, random
 
@@ -308,4 +309,4 @@ def delete_question(qid):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
